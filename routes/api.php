e4195e6d3dbb64df32b6de77b2253ffb1420d7c3
Naufal Farros <71809599+NaufalFarros@users.noreply.github.com>
2022-10-31 17:10:52 +0700

    p

--- routes/api.php
+++ routes/api.php
@@ -21,4 +21,4 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 
 
 
-Route::post('/sensor',[dataiotController::class,'store']);
+Route::post('/',[dataiotController::class,'store']);
