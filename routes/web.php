439ab912557aee5fd7f104b56b6bd5d2a69aa267
Naufal Farros <71809599+NaufalFarros@users.noreply.github.com>
2022-10-31 16:50:28 +0700

    p

--- routes/web.php
+++ routes/web.php
@@ -18,7 +18,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return redirect('/admin');
+    return view('welcome');
 });
 
 Route::get('/sensor',[dataiotController::class,'ajax']);
