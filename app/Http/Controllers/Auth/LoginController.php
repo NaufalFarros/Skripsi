a71be9de73e386857744af15071bfa279b83c0ec
Naufal Farros <71809599+NaufalFarros@users.noreply.github.com>
2022-11-01 21:43:27 +0700

    o

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -39,11 +39,11 @@ class LoginController extends Controller
 
 
      
-    public function __construct()
-    {
-        $this->middleware('guest')->except('logout');
+//     public function __construct()
+//     {
+//         $this->middleware('guest')->except('logout');
  
-   }
+//    }
 
 //    public function login(Request $request)
 //    {
