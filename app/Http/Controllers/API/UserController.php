72328644c965c65ec3bc557f9648bc6a4ae03310
Naufal Farros <71809599+NaufalFarros@users.noreply.github.com>
2022-11-22 22:38:11 +0700

    p

--- app/Http/Controllers/API/UserController.php
+++ app/Http/Controllers/API/UserController.php
@@ -8,6 +8,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
+use Laravel\Sanctum\PersonalAccessToken;
 use MongoDB\Operation\Update;
 use PhpParser\Node\Stmt\Return_;
 
@@ -120,8 +121,11 @@ class UserController extends Controller
 
     public function fetch(Request $request){
         // return $request->user();
-
-        $user = User::where('_id', $request->user()->_id)->first();
+       
+        $hashedTooken = $request;
+        $token = PersonalAccessToken::findToken($hashedTooken);
+        $user = $token->tokenable;
+        // $user = User::where('_id', $request->user()->_id)->first();
 
         return response()->json([
             'user' => $user,
