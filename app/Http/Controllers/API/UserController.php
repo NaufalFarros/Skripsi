c92b1808d6be01966d0c6a775faec4f0ce69e871
Naufal Farros <71809599+NaufalFarros@users.noreply.github.com>
2022-11-30 13:12:44 +0700

    p

--- app/Http/Controllers/API/UserController.php
+++ app/Http/Controllers/API/UserController.php
@@ -19,29 +19,29 @@ class UserController extends Controller
 {
 
     public function getImage(Request $request)
-    { 
+    {
         //get user from auth sacntum token
         $user = $request->user();
-       
 
-        if($user->profile_image == 'https://ui-avatars.com/api/?'. $user->name ){
+
+        if ($user->profile_image == 'https://ui-avatars.com/api/?' . $user->name) {
             $path = $user->profile_image;
             // dd($path);
             return response()->json($path, 200);
-        }else{
-            $path = Storage::url('avatars/'.$user->profile_image);
+        } else {
+            $path = Storage::url('avatars/' . $user->profile_image);
             // dd($path);
             return response()->json($path, 200);
         }
-        
-        
-        
-        
+
+
+
+
 
         // $user = Auth::user();
-        
+
         // $image = Storage::disk('avatars')->get($user->profile_image);
-        
+
     }
 
 
@@ -49,17 +49,19 @@ class UserController extends Controller
     public function register(Request $request)
     {
 
-        $validator = Validator::make($request->all(), [
-            'name' => ['required', 'string', 'max:255'],
-            'username' => ['required', 'string', 'max:255', 'unique:users'],
-            'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
-            'password' => ['required', 'string', 'min:6', 'confirmed'],
-        ],
+        $validator = Validator::make(
+            $request->all(),
             [
-               'username.unique' => 'Username already exists',
+                'name' => ['required', 'string', 'max:255'],
+                'username' => ['required', 'string', 'max:255', 'unique:users'],
+                'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
+                'password' => ['required', 'string', 'min:6', 'confirmed'],
+            ],
+            [
+                'username.unique' => 'Username already exists',
                 'email.unique' => 'Email already exists',
-            ] 
-    );
+            ]
+        );
 
         // if ($validator->fails()) {
         //     return response()->json($validator->errors()->toJson(), 400);
@@ -96,7 +98,8 @@ class UserController extends Controller
     }
 
 
-    public function login(Request $request){
+    public function login(Request $request)
+    {
 
         //login dengan username atau email
         $login = $request->validate([
@@ -145,38 +148,29 @@ class UserController extends Controller
                 'user' => $user
             ]
         );
-
-
-
-        
-
     }
 
-    public function fetch(Request $request){
+    public function fetch(Request $request)
+    {
 
         return response()->json([
             'user' => $request->user()
         ]);
     }
 
-    public  function updateProfile(Request $request){
+    public  function updateProfile(Request $request)
+    {
 
         // $data = $request->all();
         // dd($data);
-        $user = User::where('_id', Auth::user()->_id)->first();
-        $photoUser = $user->profile_image;
 
-        if($photoUser != null){
-            $path = public_path('storage/avatars/'.$photoUser);
-            if(File::exists($path)){
-                File::delete($path);
-            }
-        }
-        if($request->name != null){
+        if ($request->name != null) {
             //validasi data
-            $this->validate($request, [
-                'name' => ['required', 'string', 'max:255'],
-            ]
+            $this->validate(
+                $request,
+                [
+                    'name' => ['required', 'string', 'max:255'],
+                ]
             );
 
             $user = User::where('_id', Auth::user()->_id)->first();
@@ -184,11 +178,13 @@ class UserController extends Controller
             $user->save();
         }
 
-        if($request->username != null){
+        if ($request->username != null) {
             //validasi data
-            $this->validate($request, [
-                'username' => ['required', 'string', 'max:255', 'unique:users'],
-            ],
+            $this->validate(
+                $request,
+                [
+                    'username' => ['required', 'string', 'max:255', 'unique:users'],
+                ],
                 [
                     'username.unique' => 'Username already exists',
                 ]
@@ -197,19 +193,28 @@ class UserController extends Controller
             $user->username = $request->username;
             $user->save();
         }
-        
 
-        if($request->hasFile('image')){
+
+        if ($request->hasFile('image')) {
             // dd($photoUser);
+            $user = User::where('_id', Auth::user()->_id)->first();
+            $photoUser = $user->profile_image;
+
+            if ($photoUser != null) {
+                $path = public_path('storage/avatars/' . $photoUser);
+                if (File::exists($path)) {
+                    File::delete($path);
+                }
+            }
             $request->validate([
                 'image' => 'required|image|mimes:jpeg,png,jpg,gif,svg|max:2048',
             ]);
-            
+
             $strRandom = str::random(10);
             $extension = $request->image->extension();
 
-            $filename = Auth::user()->name.'-'.$strRandom . '.' . $extension;
-            $request->image->storeAs('avatars',$filename,'public');
+            $filename = Auth::user()->name . '-' . $strRandom . '.' . $extension;
+            $request->image->storeAs('avatars', $filename, 'public');
             auth()->user()->update([
                 'profile_image' => $filename
             ]);
@@ -223,35 +228,34 @@ class UserController extends Controller
         // $user->Update($data);
     }
 
-    public function updatePassword(Request $request){
-            
-            $data = $request->all();
-    
-            $user = Auth::user();
-    
-            $user->password = Hash::make($data['password']);
-    
-            $user->save();
-    
-            return response()->json([
-                'status' => 'success',
-                'message' => 'Password Updated',
-                'data' => $user
-            ]);
-    
+    public function updatePassword(Request $request)
+    {
+
+        $data = $request->all();
+
+        $user = Auth::user();
+
+        $user->password = Hash::make($data['password']);
+
+        $user->save();
+
+        return response()->json([
+            'status' => 'success',
+            'message' => 'Password Updated',
+            'data' => $user
+        ]);
     }
 
 
-    public function logout(Request $request){
-        
-       $token = $request->user()->currentAccessToken()->delete();
-        
+    public function logout(Request $request)
+    {
+
+        $token = $request->user()->currentAccessToken()->delete();
+
         return response()->json([
             'token' => $token,
             'status' => 'success',
             'message' => 'Logged Out'
         ]);
     }
-
-
 }
